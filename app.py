855528ca84250e37e97ef5fa757bfaea04e05cbb
Karan Bir <kbir@moveworks.ai>
2022-08-24 04:49:13 -0400

    Full commit of Boilerplate Slack Slash Command Redirecting App

---
+++ app.py
@@ -0,0 +1,12 @@
+import logging
+from slack_bolt import App
+
+app = App()
+
+@app.command("/<your_bot_name>")
+def hello_command(ack, body):
+    user_id = body["user_id"]
+    ack(f"Hi <@{user_id}>, you can get to <your_bot_name> from <|here>!")
+
+if __name__ == "__main__":
+    app.start(3000)
---
+++ start.sh
@@ -0,0 +1,3 @@
+# Start the application
+# pip3 install -r requirements.txt
+python3 app.py
