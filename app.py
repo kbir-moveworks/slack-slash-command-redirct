4dfdc4e8ba64664d643419bd237b9a44d7f253d9
Karan Bir <kbir@moveworks.ai>
2022-08-24 04:55:47 -0400

    Remove logging and add placeholder bot deep link

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import logging
 from slack_bolt import App
-
 app = App()
 
 @app.command("/<your_bot_name>")
 def hello_command(ack, body):
     user_id = body["user_id"]
-    ack(f"Hi <@{user_id}>, you can get to <your_bot_name> from <|here>!")
+    ack(f"Hi <@{user_id}>, you can get to <your_bot_name> from <https://your_bot_deep_link|here>!")
 
 if __name__ == "__main__":
     app.start(3000)
